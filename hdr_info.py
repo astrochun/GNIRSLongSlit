4811e34a92dab8962db06a3b0f5b922681763778
Chun Ly <astro.chun@gmail.com>
2017-03-04 22:18:07 -0700

    hdr_info.main: Re-organize and check if ASCII table output exists first

--- hdr_info.py
+++ hdr_info.py
@@ -44,31 +44,41 @@ def main(path0, silent=False, verbose=True):
     Notes
     -----
     Created by Chun Ly, 4 March 2017
+     - Later re-organized to check for file first
     '''
 
     if silent == False: log.info('### Begin main : '+systime())
 
-    fits_files = glob.glob(path0+'N*fits')
-    n_files = len(fits_files)
-
-    names0 = ('filename', 'datelabel', 'UT_date', 'obstype', 'object',
-              'exptime', 'grating', 'gratwave', 'filter1', 'filter2', 'slit')
-    dtype0 = ('S20', 'S30', 'S25', 'S8', 'S100',
-              'f8', 'S15', 'f8', 'S10', 'S10', 'S20')
-    tab0 = Table(names=names0, dtype=dtype0)
-
-    for nn in xrange(n_files):
-        basename = os.path.basename(fits_files[nn])
-        if verbose == True: log.info('## Reading : '+basename)
-        h0 = fits.getheader(fits_files[nn])
-        vec0 = [basename, h0['DATALAB'], h0['DATE-OBS']+'T'+h0['UT'],
-                h0['OBSTYPE'], h0['OBJECT'], h0['EXPTIME'], h0['GRATING'],
-                h0['GRATWAVE'], h0['FILTER1'], h0['FILTER2'], h0['SLIT']]
-        tab0.add_row(vec0)
-
     outfile = path0 + 'hdr_info.tbl'
-    if silent == False: log.info('## Writing : '+outfile)
-    asc.write(tab0, outfile, format='fixed_width_two_line')
+
+    # Mod later on 04/03/2017 to not overwrite file
+    if exists(outfile):
+        if silent == False:
+            log.warning('## File exists : '+outfile)
+            log.warning('## Not over-writing!!! ')
+    else:
+        fits_files = glob.glob(path0+'N*fits')
+        n_files = len(fits_files)
+
+        names0 = ('filename', 'datelabel', 'UT_date', 'obstype', 'object',
+                  'exptime', 'grating', 'gratwave', 'filter1', 'filter2',
+                  'slit')
+        dtype0 = ('S20', 'S30', 'S25', 'S8', 'S100',
+                  'f8', 'S15', 'f8', 'S10', 'S10', 'S20')
+        tab0 = Table(names=names0, dtype=dtype0)
+
+        for nn in xrange(n_files):
+            basename = os.path.basename(fits_files[nn])
+            if verbose == True: log.info('## Reading : '+basename)
+            h0 = fits.getheader(fits_files[nn])
+            vec0 = [basename, h0['DATALAB'], h0['DATE-OBS']+'T'+h0['UT'],
+                    h0['OBSTYPE'], h0['OBJECT'], h0['EXPTIME'], h0['GRATING'],
+                    h0['GRATWAVE'], h0['FILTER1'], h0['FILTER2'], h0['SLIT']]
+            tab0.add_row(vec0)
+
+        if silent == False: log.info('## Writing : '+outfile)
+        asc.write(tab0, outfile, format='fixed_width_two_line')
+    #endelse
 
     if silent == False: log.info('### End main : '+systime())
 #enddef
