e677e299b1d93266560c21d20ec8b583c5174be4
Chun Ly <astro.chun@gmail.com>
2017-03-10 21:57:30 -0700

    IQ_plot: Compute average line profile from median array in main() and plot profile in a separate inset in main()

--- IQ_plot.py
+++ IQ_plot.py
@@ -16,6 +16,7 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
+from pylab import subplots_adjust
 
 import glob
 
@@ -23,16 +24,13 @@ from astropy.table import Table
 from astropy import log
 
 from scipy.optimize import curve_fit
-
-import astropy.units as u
+from scipy.interpolate import interp1d
 
 pscale = 0.15 # arcseconds per pixel
 
 # From: https://www.gemini.edu/sciops/telescopes-and-sites/observing-condition-constraints
 FWHM_IQ_C = ['20%', '70%', '85%', 'any']
 FWHM_IQ_J = [0.40, 0.60, 0.85, 1.40]
-
-from pylab import subplots_adjust
 
 def gauss1d(x, a0, a, x0, sigma):
     return a0 + a * np.exp(-(x - x0)**2 / (2 * sigma**2))
@@ -49,7 +47,6 @@ def compute_fwhm(im0):
     im0 : numpy array
       Array from fits.getdata() of the FITS image
 
-
     Returns
     -------
     bins : numpy array
@@ -61,6 +58,7 @@ def compute_fwhm(im0):
     Notes
     -----
     Created by Chun Ly, 10 March 2017
+     - Later modified to shift each median profile and return the array
     '''
 
     # First find the peak of the nearby star
@@ -77,9 +75,12 @@ def compute_fwhm(im0):
     stack0 = np.zeros( (len(bins),len(cols)) )
     fwhm0 = np.zeros(len(bins))
 
+    stack0_shift = np.zeros( (len(bins),len(cols)/2) ) # Later + on 10/03/2017
+
     for bb in xrange(len(bins)):
-        temp = im0_crop[bins[bb]:bins[bb]+bsize-1,:]
-        stack0[bb,:] = np.median(temp, axis=0)
+        temp  = im0_crop[bins[bb]:bins[bb]+bsize-1,:]
+        t_med = np.median(temp, axis=0)
+        stack0[bb,:] = t_med - np.median(t_med)
 
         x = np.arange(len(cols))
         y = stack0[bb,:].reshape(len(cols))
@@ -87,7 +88,13 @@ def compute_fwhm(im0):
         popt, pcov = curve_fit(gauss1d, x, y, p0=p0)
         fwhm0[bb] = popt[3]*2*np.sqrt(2*np.log(2)) * pscale
 
-    return bins+bsize/2, fwhm0
+        # Later + on 10/03/2017
+        x_shift = x - popt[2]
+        f = interp1d(x_shift, y)
+        y_shift = f(range(-25,25))
+        stack0_shift[bb,:] = y_shift/max(y_shift)
+
+    return bins+bsize/2, fwhm0, stack0_shift
 
 def main(path0='', out_pdf='', check_quality=True, silent=False, verbose=True):
     '''
@@ -120,6 +127,7 @@ def main(path0='', out_pdf='', check_quality=True, silent=False, verbose=True):
     -----
     Created by Chun Ly, 10 March 2017
      - Later modified to include check_quality keyword option
+     - Later modified to include inset that shows the stacked line profile
     '''
 
     if silent == False: log.info('### Begin main : '+systime())
@@ -143,7 +151,9 @@ def main(path0='', out_pdf='', check_quality=True, silent=False, verbose=True):
         hdr0 = fits.getheader(path0+files[nn])
         im0  = fits.getdata(path0+files[nn])
 
-        bins, fwhm0 = compute_fwhm(im0)
+        bins, fwhm0, stack0_shift = compute_fwhm(im0)
+
+        # if nn == 0: fits.writeto(path0+'stack0_shift.fits', stack0_shift)
 
         row = nn % 2
         if row == 0: fig, ax0 = plt.subplots(2, 1)
@@ -151,6 +161,28 @@ def main(path0='', out_pdf='', check_quality=True, silent=False, verbose=True):
         good = np.where(fwhm0 >0)[0]
         ax0[row].plot(bins[good], fwhm0[good], marker='o', alpha=0.5,
                       mec='none', mfc='b', linestyle='none', zorder=2)
+
+        # Compute average line profile from stack0_shift
+        # Later + on 10/03/2017
+        avg_stack = np.average(stack0_shift, axis=0)
+        x0_avg    = np.arange(-25,25)
+        if row == 0: axi = fig.add_axes([0.60,0.81,0.25,0.15])
+        if row == 1: axi = fig.add_axes([0.60,0.36,0.25,0.15])
+        axi.plot(x0_avg*pscale, avg_stack, 'k-')
+        axi.set_ylim([-0.4,1.1])
+        axi.set_xlim([-2.0,2.0])
+        axi.set_xticks(range(-2,2,1))
+        axi.set_xlabel('X [arcsec]', fontsize=8)
+        axi.tick_params(labelsize=8)
+        axi.minorticks_on()
+        p0 = [0.0, 1.0, 0.0, 2.0]
+        popt, pcov = curve_fit(gauss1d, x0_avg, avg_stack, p0=p0)
+        avg_fwhm0 = popt[3]*2*np.sqrt(2*np.log(2)) * pscale
+        axi.plot(x0_avg*pscale, gauss1d(x0_avg, *popt), 'r--')
+        axi.annotate('FWHM = %.3f"' % avg_fwhm0, [0.50,0.025],
+                     xycoords='axes fraction', ha='center', va='bottom')
+        ax0[row].axhline(y=avg_fwhm0, linewidth=2, color='r',
+                         linestyle='--', zorder=1)
 
         # Median FWHM | Later + on 10/03/2017
         med_fwhm0 = np.median(fwhm0[good])
@@ -194,7 +226,7 @@ def main(path0='', out_pdf='', check_quality=True, silent=False, verbose=True):
 
         # Aesthetics
         ax0[row].set_xlim([0,1050])
-        ax0[row].set_ylim([min(fwhm0)-0.05,max(fwhm0)+0.05])
+        ax0[row].set_ylim([min(fwhm0)-0.025,max(fwhm0)+0.075])
         ax0[row].minorticks_on()
 
         if row == 1 or nn == n_files-1:
