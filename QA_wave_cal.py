4ac372a632ba767c8df144c3ee6714f5c6594db1
Chun Ly <astro.chun@gmail.com>
2017-05-18 23:46:28 -0700

    QA_wave_cal.arc_check: Add code to plot arc data; Output PDF file

--- QA_wave_cal.py
+++ QA_wave_cal.py
@@ -15,11 +15,12 @@ import glob
 import numpy as np
 
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 
 from astropy.io import fits
 from astropy import log
 
-def arc_check(path, arcs='', silent=False, verbose=True):
+def arc_check(path, arcs='', out_pdf='', silent=False, verbose=True):
 
     '''
     Generate plot illustrating wavelength calibration from IRAF database
@@ -54,12 +55,18 @@ def arc_check(path, arcs='', silent=False, verbose=True):
     d_files   = [path+'database/idwrnc'+file0.replace('.fits','_SCI_1_') \
                  for file0 in arcs]
 
+    out_pdf = path+'arc_check.pdf' if out_pdf == '' else path+out_pdf
+    pp = PdfPages(out_pdf)
+
     for nn in xrange(n_arcs):
-        print rnc_files[nn]
         hdu0 = fits.open(rnc_files[nn])
         im0  = hdu0['SCI'].data
-        f    = open(d_files[nn], 'r')
-        str0 = f.readlines()
+
+        fig, ax = plt.subplots()
+        ax.imshow(im0, cmap='Greys', origin='lower')
+
+        f0   = open(d_files[nn], 'r')
+        str0 = f0.readlines()
 
         beg_mark  = [ii for ii in xrange(len(str0)) if 'begin' in str0[ii]]
         feat_mark = [ii for ii in xrange(len(str0)) if 'features' in str0[ii]]
@@ -83,11 +90,19 @@ def arc_check(path, arcs='', silent=False, verbose=True):
                 y_val[cc,rr]  = np.float(temp1[1])
                 l_val0[cc,rr] = np.float(temp1[2])
                 l_val[cc,rr]  = np.float(temp1[3])
+            #endfor
 
-        #print len(im0[0])
-        plt.imshow(im0, cmap='Greys', origin='lower')
+            x_temp = np.repeat(x_cols[cc],n_features[cc])
+            y_temp = y_val[cc,0:n_features[cc]-1]
+            print len(x_temp), len(y_temp)
+            ax.scatter(x_temp, y_temp, color='red', marker='o', alpha=0.5)
+        #endfor
+
+        fig.set_size_inches(11,8)
+        fig.savefig(pp, format='pdf')
     #endfor
 
+    pp.close()
     if silent == False: log.info('### End QA_wave_cal : '+systime())
 #enddef
 
