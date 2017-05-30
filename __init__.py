b5311bb78bdab3dcfa6c08f8ec8511ee10d53562
Chun Ly <astro.chun@gmail.com>
2017-05-30 16:03:32 -0700

    Include DEEP15 in gnirs_2017a array

--- __init__.py
+++ __init__.py
@@ -18,7 +18,7 @@ import GNIRSLongSlit as gnirsls
 """
 
 # + on 12/03/2017
-gnirs_2017a = ['DEEP05', 'DEEP06', 'DEEP07', 'DEEP10', 'DEEP14',
+gnirs_2017a = ['DEEP05', 'DEEP06', 'DEEP07', 'DEEP10', 'DEEP14', 'DEEP15',
                'Keck03', 'Keck14', 'Keck27', 'MMT08', 'MMT37']
 
 __all__ = [gnirs_2017a]
