ef322d09aa02db258947d20075a71c81f8675848
Chun Ly <astro.chun@gmail.com>
2017-06-22 16:17:18 -0700

    chevron_fix.run: Compute statistics to determine which pixels to apply bias correction

--- chevron_fix.py
+++ chevron_fix.py
@@ -45,6 +45,10 @@ def run(files=[''], file_list='', n_rows=1, silent=False, verbose=True):
     Notes
     -----
     Created by Chun Ly, 17 June 2017
+    Modified by Chun Ly, 22 June 2017
+     - Compute median in each quadrant
+     - Apply correction within a certain number of std deviations (handles random noise
+       but not regions with low emission from sky)
     '''
     
     if silent == False:
@@ -64,22 +68,51 @@ def run(files=[''], file_list='', n_rows=1, silent=False, verbose=True):
     for ff in xrange(len(files)):
         hdu0 = fits.open(files[ff])
         im0  = hdu0[1].data
-
+        sh0  = im0.shape
         y_bins = np.int(np.ceil(np.float(im0.shape[0])/n_rows))
-        med0_arr = np.zeros(im0.shape)
+        med0_arr = np.zeros(sh0)
+
+        # Compute median for each quadrant | + on 22/06/2017
+        median0 = np.zeros(4)
+        mean0   = np.zeros(4)
+        std0    = np.zeros(4)
+        r1 = [sh0[0]/2, sh0[0]/2,        0,        0]
+        r2 = [sh0[0],   sh0[0],   sh0[0]/2, sh0[0]/2]
+        c1 = [       0, sh0[1]/2,        0, sh0[1]/2]
+        c2 = [sh0[1]/2, sh0[1],   sh0[1]/2, sh0[1]]
+        for qq in range(4):
+            t_im0 = im0[r1[qq]:r2[qq],c1[qq]:c2[qq]]
+            mean, median, std = sigma_clipped_stats(t_im0, sigma=2, iters=100)
+            mean0[qq]   = mean
+            median0[qq] = median
+            std0[qq]    = std
+            log.info('## q'+str(qq+1)+' %.3f %.3f %3f ' % (mean, median, std))
+
         for row in range(y_bins):
-            mean, median, std = sigma_clipped_stats(im0[row], sigma=2, iters=10, axis=0)
+            # mean, median, std = sigma_clipped_stats(im0[row], sigma=2, iters=10, axis=0)
             stat0, edge, num = binned_statistic(x0, im0[row], bins=bins,
                                                 statistic='median')
 
-            med0_arr[row] = np.repeat(stat0-median, 8)
+            # Determine median and std to adopt | + on 22/06/2017
+            if row >= sh0[0]/2:
+                t_med = np.repeat(median0[0:2], sh0[1]/2)
+                t_std = np.repeat(std0[0:2], sh0[1]/2)
+            if row < sh0[0]/2:
+                t_med = np.repeat(median0[2:4], sh0[1]/2)
+                t_std = np.repeat(std0[2:4], sh0[1]/2)
+
+            # Compute offsets | + on 22/06/2017
+            med0_arr[row] = np.repeat(stat0, 8) - t_med
+            no_fix = np.where(np.absolute(med0_arr[row]/t_std) >= 5)[0]
+            if len(no_fix) > 0: med0_arr[row,no_fix] = 0.0
         #endfor
 
         corr_hdu = hdu0
         corr_im0 = im0 - med0_arr
         corr_hdu[1].data = corr_im0
 
-        corr_hdu.writeto(file0.replace('.fits','.bcorr.fits'), overwrite=True)
+        corr_hdu.writeto(files[ff].replace('.fits','.bcorr.fits'), overwrite=True)
+        fits.writeto(files[ff].replace('.fits','.fix.fits'), med0_arr, overwrite=True)
     #endfor
 
     if silent == False:
