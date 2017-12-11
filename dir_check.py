3d72a73ceb5724fb2bcdbdca9665ce6a15f5bdf5
Chun Ly <astro.chun@gmail.com>
2017-12-10 20:36:13 -0700

    dir_check.main: Implement glog logging, allow mylogger keyword input

--- dir_check.py
+++ dir_check.py
@@ -25,7 +25,7 @@ def is_date(string):
     except ValueError:
         return False
 
-def main(path0, silent=False, verbose=True):
+def main(path0, mylogger=None, silent=False, verbose=True):
     '''
     Main function to get list of directories formatted as
     YYYYMMDD
@@ -54,9 +54,17 @@ def main(path0, silent=False, verbose=True):
     Created by Chun Ly, 23 March 2017
     Modified by Chun Ly, 18 June 2017
      - Change dir_list output if no directories is available
+    Modified by Chun Ly, 10 December 2017
+    - Implement glog logging, allow mylogger keyword input
     '''
 
-    if silent == False: log.info('### Begin main : '+systime())
+    # + on 10/12/2017
+    if type(mylogger) == type(None):
+        mylog, clog = 0, log
+    else:
+        mylog, clog = 1, mylogger
+
+    if silent == False: clog.info('### Begin main : '+systime())
 
     list0 = [os.path.join(path0,f) for f in os.listdir(path0)]
     dirs = filter(os.path.isdir, list0)
@@ -71,15 +79,20 @@ def main(path0, silent=False, verbose=True):
     # Later + on 23/03/2017
     if len(dir_list) == 0:
         if silent == False:
-            log.info('## No dir found')
+            clog.info('No dir found') # Mod on 10/12/2017
+
         dir_list = [''] # Mod on 18/07/2017
         list_path = [path0]
     else:
         if silent == False:
-            log.info('## The following date dir found: '+', '.join(dir_list))
+            # Mod on 10/12/2017
+            txt0 = 'The following date dir found: '+', '.join(dir_list)
+            if not mylog: txt0 = '## '+txt0
+            clog.info(txt0)
+
         list_path = [path0+a+'/' for a in dir_list]
 
-    if silent == False: log.info('### End main : '+systime())
+    if silent == False: clog.info('### End main : '+systime()) # Mod on 10/12/2017
     return dir_list, list_path
 #enddef
 
