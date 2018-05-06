55d0f6c140774ed045bcaeb1d7a11b4a1e34f2fd
Chun Ly <astro.chun@gmail.com>
2018-05-05 23:15:29 -0700

    examine_median.run: Write skysub statistics ASCII table

--- examine_median.py
+++ examine_median.py
@@ -12,6 +12,7 @@ from chun_codes import systime
 
 from os.path import exists
 from astropy.io import fits
+from astropy.io import ascii as asc
 
 import numpy as np
 
@@ -22,6 +23,7 @@ import glob
 from check_path import main as check_path
 
 from astropy import log
+from astropy.table import Table
 
 import glog
 
@@ -71,6 +73,8 @@ def run(rawdir, style, mylogger=None, silent=False, verbose=True):
      - Plot aesthetics (axis labels, borders)
      - Write median-remove skysub images
      - Modify plotting to handle style != 'skysub' (single panel)
+    Modified by Chun Ly, 5 May 2018
+     - Write skysub statistics ASCII table
     '''
 
     # + on 18/12/2017
@@ -91,8 +95,8 @@ def run(rawdir, style, mylogger=None, silent=False, verbose=True):
 
     infile = rawdir+'obj.lis'
     if silent == False: clog.info('Reading : '+infile)
-    files0 = np.loadtxt(infile, dtype=type(str)).tolist()
-    files0 = [rawdir+prefix+file0 for file0 in files0]
+    files1 = np.loadtxt(infile, dtype=type(str)).tolist()
+    files0 = [rawdir+prefix+file1 for file1 in files1]
 
     # + on 15/09/2017
     if style == 'flat':
@@ -102,6 +106,11 @@ def run(rawdir, style, mylogger=None, silent=False, verbose=True):
         fig, ax_arr = plt.subplots(nrows=2)
     else:
         fig, ax_arr = plt.subplots()
+
+    if style == 'skysub': # + on 05/05/2018
+        arr_mean = np.zeros(len(files0))
+        arr_med  = np.zeros(len(files0))
+        arr_sig  = np.zeros(len(files0))
 
     for ii in range(len(files0)):
         hdu0   = fits.open(files0[ii]) # Mod on 30/04/2018
@@ -119,6 +128,11 @@ def run(rawdir, style, mylogger=None, silent=False, verbose=True):
             ax_arr[0].axhline(y=c_med0, color='black', linestyle='--',
                               linewidth=0.5)
 
+            # + on 05/05/2018
+            arr_mean[ii] = c_mean0
+            arr_med[ii]  = c_med0
+            arr_sig[ii]  = c_sig0
+
             im1  = im0 - c_med0
             med0 = np.median(im1, axis=0)
             ax_arr[1].plot(med0, label=label0, linewidth=0.5)
@@ -133,7 +147,6 @@ def run(rawdir, style, mylogger=None, silent=False, verbose=True):
             hdu1['SCI'].data = im1
             clog.info('Write median-removed skysub file : '+files0[ii])
             hdu1.writeto(files0[ii], overwrite=True)
-
         #endif
     #endfor
 
@@ -148,6 +161,14 @@ def run(rawdir, style, mylogger=None, silent=False, verbose=True):
 
         ax_arr[0].set_ylabel('Flux')
         ax_arr[1].set_ylabel('Flux')
+
+        # + on 05/05/2018
+        t_arr0 = [files1, arr_mean, arr_med, arr_sig]
+        tab0 = Table(t_arr0, names=('File','sky_mean','sky_med','sky_sig'))
+        sky_stats_outfile = prefix+'sky_stats.tbl'
+        if silent == False:
+            clog.info('Writing : '+sky_stats_outfile)
+        asc.write(tab0, sky_stats_outfile, format='fixed_width_two_line')
     else:
         ax_arr.legend(fontsize=6, frameon=False)
         ax_arr.set_xlabel('X [pixels]')
